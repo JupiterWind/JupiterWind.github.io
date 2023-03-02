bb626e49c83aa07772c66fd1be323191ae742fbb
JupiterWind <hasiyara@naver.com>
2023-03-03 08:31:14 +0900

    블러수정

--- static/js/script.js
+++ static/js/script.js
@@ -1,54 +1,56 @@
-window.onload = function (){
+window.onload = function () {
+  // 우클릭 방지
+  document.oncontextmenu = function () {
+    return false;
+  };
 
-    // 우클릭 방지
-    document.oncontextmenu = function(){ return false; }
-    
-    window.addEventListener('scroll', handleScroll);
+  window.addEventListener("scroll", handleScroll);
 
-    // 랜덤 배경이미지 
-    handleBgImage();
-    
-}
+  // 랜덤 배경이미지
+  handleBgImage();
+};
 
 function handleBgImage() {
   let randomNumber = Math.floor(Math.random() * 15) + 1;
-    document.querySelector('header img').setAttribute('src', 'static/img/bg0' + randomNumber + '.jpg');
-  }
+  document
+    .querySelector("header img")
+    .setAttribute("src", "static/img/bg0" + randomNumber + ".jpg");
+}
 
-// 스크롤 애니메이션  
+// 스크롤 애니메이션
 function handleScroll() {
-    const elems = document.querySelectorAll('.up-on-scroll');
-    elems.forEach(elem => {
-      if (isElementUnderBottom(elem, -20)) {
-        elem.style.opacity = "0";
-        elem.style.transform = 'translateY(7rem)';
-      } else {
-        elem.style.opacity = "1";
-        elem.style.transform = 'translateY(0px)';
-      }
-    })
+  const elems = document.querySelectorAll(".up-on-scroll");
+  elems.forEach((elem) => {
+    if (isElementUnderBottom(elem, -20)) {
+      elem.style.opacity = "0";
+      elem.style.transform = "translateY(7rem)";
+    } else {
+      elem.style.opacity = "1";
+      elem.style.transform = "translateY(0px)";
+    }
+  });
 
-    const elems_show = document.querySelectorAll('.show-on-scroll');
-    elems_show.forEach(elem => {
-      if (isElementInViewport(elem)) {
-        elem.style.opacity = "1"; 
-      } else {
-        elem.style.opacity = "0";
-      }
-    })
+  const elems_show = document.querySelectorAll(".show-on-scroll");
+  elems_show.forEach((elem) => {
+    if (isElementInViewport(elem)) {
+      elem.style.opacity = "1";
+    } else {
+      elem.style.opacity = "0";
+    }
+  });
 
-    const elems_effect = document.querySelectorAll('.effect-on-scroll');
-    elems_effect.forEach(elem => {
-      if (isElementInViewport(elem)) {
-        elem.classList.add('is_visible');
-        elem.style.transform = ' scale(1)';
-        elem.style.filter = 'blur(0rem)';
-      } else {
-        elem.classList.remove('is_visible');
-        elem.style.transform = ' scale(2)';
-        elem.style.filter = 'blur(1rem)';
-      }
-    })
+  const elems_effect = document.querySelectorAll(".effect-on-scroll");
+  elems_effect.forEach((elem) => {
+    if (isElementInViewport(elem)) {
+      elem.classList.add("is_visible");
+      elem.style.transform = " scale(1)";
+      elem.style.filter = "blur(0rem)";
+    } else {
+      elem.classList.remove("is_visible");
+      elem.style.transform = " scale(2)";
+      elem.style.filter = "blur(0.3rem)";
+    }
+  });
 }
 
 // 화면이 보여지는지 확인하는 함수
@@ -66,15 +68,13 @@ function isElementInViewport(el) {
   }
   var rect = el.getBoundingClientRect();
   return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
     (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
   );
 }
-  
- 
